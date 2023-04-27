1ad13c838e76411503adff2b5f1c4aa12533308a
faithfulnesss <torfik1med@gmail.com>
2023-04-27 03:43:27 +0000

    Created Poll and Choice model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    poll_question = models.CharField(max_length=255)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.poll_question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
