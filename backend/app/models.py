378492025fd69f97026d11873b018f40cd864947
faithfulnesss <torfik1med@gmail.com>
2023-05-05 05:11:24 +0000

    Update models definition

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Poll(models.Model):
     poll_question = models.CharField(max_length=255)
     publication_date = models.DateTimeField(auto_now_add=True)
+    poll_description = models.TextField(blank=True) 
 
     def __str__(self):
         return self.poll_question
@@ -11,7 +12,11 @@ class Poll(models.Model):
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    user = models.ForeignKey()
+    poll = models.ForeignKey(Poll)
+    choice = models.ForeignKey(Choice)
