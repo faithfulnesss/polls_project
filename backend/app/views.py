4d47b4e0165239c9fae54a1d5639b2cad248320d
faithfulnesss <torfik1med@gmail.com>
2023-05-07 11:30:37 +0000

    Tried to display the html with tailwind styles using the views and pass the context there

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from app.models import Poll
 # Create your views here.
+
+def test_view(request):
+    poll = Poll(poll_question = 'aboba')
+    poll.save()
+    return render(request, "index.html", {"poll" : poll})
