e6b5dcd3c31acbd4cf9e164e657775a4e274b159
faithfulnesss <torfik1med@gmail.com>
2023-05-07 11:29:57 +0000

    Updated foreign key constrains

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Poll(models.Model):
@@ -17,6 +18,6 @@ class Choice(models.Model):
         return self.choice_text
 
 class Vote(models.Model):
-    user = models.ForeignKey()
-    poll = models.ForeignKey(Poll)
-    choice = models.ForeignKey(Choice)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
